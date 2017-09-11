c8ec9f131d6627c4f4cba22af0b09b7cc64c591b
THIAGO ROCHA EINLOFT <631520227@fspoaeduc.com.br>
2017-09-11 19:52:39 -0300

    Adicionando a classe de venda

---
+++ src/Venda.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Venda {
+	private int codigo;
+	private LocalDateTime hora;
+	private LocalDateTime data;
+	private Cliente cliente;
+	private Produto produto;
+	
+	
+	/*
+	 * Criar um método:
+	 * 
+	 * -Metodo de venda do produto
+	 * quantidade * preço
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	
+	
+	
+}
